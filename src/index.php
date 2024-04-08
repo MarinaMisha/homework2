6e64c15435ae505915f1686b920c845a3b03c9af
MarinaMisha <marinagalizdra2@gmail.com>
2024-04-08 21:05:24 +0300

    first commit

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Hello. What is you name?";
+$name = trim(fgets(STDIN));
+ echo "$name How old are you?";
+ $age = trim(fgets(STDIN));
+ echo "$name $age Where you from";
+ $from = trim(fgets(STDIN));
+ echo "$name $age $from It's true that you study programming?";
+ $study = trim(fgets(STDIN));
+ echo "name = $name , age = $age , from = $from , study = $study";
+//$number1 = 24;
+//$number2 = 37;
+//$result = ($number1 + $number2) / 2;
+  //echo $result . PHP_EOL;
