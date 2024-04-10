30c6d9491c1de7742b280420cc3b8533154cb4d0
MarinaMisha <marinagalizdra2@gmail.com>
2024-04-10 23:41:22 +0300

    2md commit

--- src/index.php
+++ src/index.php
@@ -1,15 +1,85 @@
 <?php
 
-echo "Hello. What is you name?";
-$name = trim(fgets(STDIN));
- echo "$name How old are you?";
- $age = trim(fgets(STDIN));
- echo "$name $age Where you from";
- $from = trim(fgets(STDIN));
- echo "$name $age $from It's true that you study programming?";
- $study = trim(fgets(STDIN));
- echo "name = $name , age = $age , from = $from , study = $study";
+//echo "Hello. What is you name?";
+//$name = trim(fgets(STDIN));
+ //echo "$name How old are you?";
+ //$age = trim(fgets(STDIN));
+ //echo "$name $age Where you from";
+ //$from = trim(fgets(STDIN));
+ //echo "$name $age $from It's true that you study programming?";
+ //$study = trim(fgets(STDIN));
+ //cho "name = $name , age = $age , from = $from , study = $study";
+
 //$number1 = 24;
 //$number2 = 37;
 //$result = ($number1 + $number2) / 2;
   //echo $result . PHP_EOL;
+
+//boolean
+echo "example1" . PHP_EOL;
+$bool1 = true;
+$bool2 = false;
+$result1 = ($bool1 === $bool2) ? "Yes" : "No";
+echo $result1 . PHP_EOL;
+$result2 = ($bool1 == $bool2) ? "Yes" : "No";
+echo $result2 . PHP_EOL;
+echo "example2" . PHP_EOL;
+$num1 = 5;
+$num2 = 5;
+$result3 = ($num1 === $num2) ? "Yes" : "No";
+echo $result3 . PHP_EOL;
+$result4 = ($num1 == $num2) ? "Yes" : "No";
+echo $result4 . PHP_EOL;
+
+echo "example3" . PHP_EOL;
+$numStr = "3";
+$num3 = 3;
+$result5 = ($numStr === $num3) ? "Yes" : "No";
+echo $result5 . PHP_EOL;
+$result6 = ($numStr == $num3) ? "Yes" : "No";
+echo $result6 . PHP_EOL;
+
+echo "example4" . PHP_EOL;
+$nullStr = null;
+$str2 = "";
+$result7 = ($nullStr === $str2) ? "Yes" : "No";
+echo $result7 . PHP_EOL;
+$result8 = ($nullStr == $str2) ? "Yes" : "No";
+echo $result8 . PHP_EOL;
+
+echo "example5" . PHP_EOL;
+$nullStr2 = null;
+$bool3 = true;
+$result9 = ($nullStr2 === $bool3) ? "Yes" : "No";
+echo $result9 . PHP_EOL;
+$result10 = ($nullStr2 == $bool3) ? "Yes" : "No";
+echo $result10 . PHP_EOL;
+
+echo "example6" . PHP_EOL;
+$str1 = "Hello";
+$str2 = "Marina";
+$result11 = ($str1 === $str2) ? "Yes" : "No";
+echo $result11 . PHP_EOL;
+$result12 = ($str1 == $str2) ? "Yes" : "No";
+echo $result12 . PHP_EOL;
+
+echo "example7" . PHP_EOL;
+$array1 = [];
+$str4 = "";
+$result13 = ($array1 == $str4) ? "Yes" : "No";
+echo $result13 . PHP_EOL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
