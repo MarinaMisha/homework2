a94ac5898d8c27f884a1da9ddb4422cf1eda5a0f
MarinaMisha <marinagalizdra2@gmail.com>
2024-04-21 14:31:05 +0300

    hom10

--- src/index.php
+++ src/index.php
@@ -1,28 +1,46 @@
 <?php
 
-function generateRandomArray( int $length , int $min , int $max) : array
+//function generateRandomArray( int $length , int $min , int $max) : array
+//{
+   // $randomArray = [] ;
+    //for ($i = 0; $i < $length ; $i++) {
+        //$randomArray[] = rand($min , $max);
+    //}
+
+
+
+    //return $randomArray;
+
+//}
+//$randomNumbers = generateRandomArray(20 , 2 , 200) ;
+////$minNumber = min($randomNumbers);
+//$maxNumber = max($randomNumbers);
+
+//echo "Масив случайних чисел: " .implode(", " , $randomNumbers) . " " .  PHP_EOL ;
+
+//echo "Минимальний єлемент: " . $minNumber . " "  . PHP_EOL;
+
+//echo "Максимальний єлемент: " . $maxNumber . " " . PHP_EOL ;
+
+//sort($randomNumbers);
+//echo "Сортированний масив: " . implode(" ," , $randomNumbers) ;
+
+function  fibonacciGenerator( int $maxNumbers): Generator
 {
-    $randomArray = [] ;
-    for ($i = 0; $i < $length ; $i++) {
-        $randomArray[] = rand($min , $max);
+    $a = 0 ;
+    $b = 1 ;
+    while ($a < $maxNumbers) {
+        yield $a ;
+        $sum = $a + $b ;
+        $a = $b ;
+        $b = $sum;
+
     }
 
-
-
-    return $randomArray;
-
 }
-$randomNumbers = generateRandomArray(20 , 2 , 200) ;
-$minNumber = min($randomNumbers);
-$maxNumber = max($randomNumbers);
-
-echo "Масив случайних чисел: " .implode(", " , $randomNumbers) . " " .  PHP_EOL ;
-
-echo "Минимальний єлемент: " . $minNumber . " "  . PHP_EOL;
-
-echo "Максимальний єлемент: " . $maxNumber . " " . PHP_EOL ;
-
-sort($randomNumbers);
-echo "Сортированний масив: " . implode(" ," , $randomNumbers) ;
+$maxNumbers = 100;
+foreach (fibonacciGenerator($maxNumbers) as $number) {
+    echo $number . " " . PHP_EOL ;
+}
 
 
