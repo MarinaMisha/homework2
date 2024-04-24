c2e1c6821660ffd82eed8b91817df0703e896540
MarinaMisha <marinagalizdra2@gmail.com>
2024-04-24 20:36:11 +0300

    hom11#

--- src/index.php
+++ src/index.php
@@ -1,46 +1,20 @@
 <?php
-
-//function generateRandomArray( int $length , int $min , int $max) : array
-//{
-   // $randomArray = [] ;
-    //for ($i = 0; $i < $length ; $i++) {
-        //$randomArray[] = rand($min , $max);
-    //}
+$g = 1;
+$a = 3;
 
 
 
-    //return $randomArray;
 
-//}
-//$randomNumbers = generateRandomArray(20 , 2 , 200) ;
-////$minNumber = min($randomNumbers);
-//$maxNumber = max($randomNumbers);
-
-//echo "Масив случайних чисел: " .implode(", " , $randomNumbers) . " " .  PHP_EOL ;
-
-//echo "Минимальний єлемент: " . $minNumber . " "  . PHP_EOL;
-
-//echo "Максимальний єлемент: " . $maxNumber . " " . PHP_EOL ;
-
-//sort($randomNumbers);
-//echo "Сортированний масив: " . implode(" ," , $randomNumbers) ;
-
-function  fibonacciGenerator( int $maxNumbers): Generator
-{
-    $a = 0 ;
-    $b = 1 ;
-    while ($a < $maxNumbers) {
-        yield $a ;
-        $sum = $a + $b ;
-        $a = $b ;
-        $b = $sum;
-
-    }
-
+function create( int $g,  int $a): void {
+    $file = fopen("log.txt" , "a");
+    fwrite($file, $g. " " .$a);
+    fclose($file);
 }
-$maxNumbers = 100;
-foreach (fibonacciGenerator($maxNumbers) as $number) {
-    echo $number . " " . PHP_EOL ;
-}
-
-
+create( 1, 3);
+echo "Аргументи били успешно записани  в log.txt\n";
+$file = fopen("log.txt" , "r");
+$log = fread($file, filesize("log.txt"));
+$logLines = explode("\n", trim($log));
+$lastLines = end($logLines) ;
+echo "Последние аргументи , которие били введени: " . $lastLines."\n";
+fclose($file);
