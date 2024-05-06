04225063a39471a1215d90d50813161697d1c2c8
MarinaMisha <marinagalizdra2@gmail.com>
2024-05-06 11:03:53 +0300

    homework890

--- src/index.php
+++ src/index.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once "ParentClass.php";
-require_once  "ChildClass.php";
-require_once "main.php";
-
+require_once "Figure.php";
+require_once "Rectangle.php";
+require_once "Circle.php";
+require_once  "main.php";
 
 
 
